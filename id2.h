9444a95a2bf062527d92cd45e58aac2d8ea196cd
umount1 <umount@gmx.net>
2018-01-10 18:21:31 +0100

    second copy of iindex.html

---
+++ id2.h
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head> 
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./myStyle.css" type="text/css" >
+</head>
+<body onload="init()">
+  <nav id="navbar"></nav>
+  <article id="main" name="main">
+    <h1>Programm zum Umrechnen von Zahlen</h1>
+    <section id="s1"><!-- Anleitung --></section>
+    <section id="s2"><!-- Eingabe --></section>
+    <section id="s3"><!-- Ausgabe --></section>
+    <section id="s4"><!-- Ausgabe --></section>
+  </article>
+  <script src="./myJS.js"></script>
+  <script src="./myJS_2.js"></script>
+  <script src="./myJS_3.js"></script>
+</body>
+</html>
